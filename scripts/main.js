d75581c47870537e72d0353bfcac9676e5da45e7
Qian Lu <techwriter.chocolate@gmail.com>
2020-08-27 12:04:09 +0200

    Added JavaScript and updated Works page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,52 @@
-function changeImage() {
+/*function showMindmap() {
 
-        document.getElementById("fullChocolate").src == "../images/chocolate-bite.png"
+        off = document.getElementById("intro-works);
+        on = document.getElementById("mindmapContent");
+        if(on.style.display == "none")
+            {
+                off.style.display = "none";
+                on.style.display = "block";
+            }
+        else
+            {
+                off.style.display = "block";
+                on.style.display = "none";
+            }*/
+
+function show(param_div_id) {
+    document.getElementById('intro-works').innerHTML = document.getElementById(param_div_id).innerHTML;
+  }
+
+
+
+function biteChocolate() {
+    var fullChocolate = document.getElementById("fullChocolate");
+    var chocolateBite = document.createElement("chocolateBite");
+    fullChocolate.parentNode.replaceChild(chocolateBite, fullChocolate);
+    /*var off = document.getElementById("fullChocolate);
+    var on = document.getElementById("chocolateBite");
+            if(on.style.display == "none")
+                {
+                    off.style.display = "none";
+                    on.style.display = "inline-block";
+                }
+            else
+                {
+                    off.style.display = "inline-block";
+                    on.style.display = "none";
+
+                }  */
+
+}
+
+
+/*function biteChocolate() {
+    var chocolateBite = document.createElement{"button"};
+    chocolateBite.id = "chocolateBite";
+    var fullChocolate = document.getElementById("fullChocolate");
+    var parentButton = fullChocolate.parentNode;
+    parentButton.replaceChild(chocolateBite, fullChocolate)
+    
+}  */
+
+
